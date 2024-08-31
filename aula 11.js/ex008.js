2b6cae322c271750fb54df22b95be091da908978
Nara Tinoco <naratinoco3@gmail.com>
2024-08-31 19:34:35 -0300

    Aula 11 = ex08 e ex09

---
+++ aula 11.js/ex008.js
@@ -0,0 +1,10 @@
+
+var vel = 78; 
+console.log(`A velocidade do seu carro é ${vel}Km/h`);
+
+if(vel>60){ // condição simples: pq ela não tem Else(senão)
+    console.log(`Você ultrapassou a velocidade permitida: multado!`);
+}
+console.log(`Dirija sempre usando o cinto de segurança`); 
+
+
